01bcef2d12bc2d6791621480687338e20989f0cf
ppp <ppp.3ppj@gmail.com>
2023-03-04 17:46:21 +0700

    create role seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,11 @@
 admin = User.find_by(email: 'admin@project.com')
 user = User.find_by(email: 'user@project.com')
 
+# example role
+[:admin, :author, :contact, :user].each do |role|
+  Role.where({ name: role }, without_protection: true).first_or_create
+end
+
 if admin.blank?
   # admin = User.create(email: 'admin@project.com', password: 'password', password_confirmation: 'password', first_name: 'admin', title: 'Mr', confirmed_at: Time.now.utc)
   admin = User.create(email: 'admin@project.com', password: 'password', password_confirmation: 'password', confirmed_at: Time.now.utc)
@@ -16,5 +21,8 @@ if user.blank?
   user = User.new(email: 'user@project.com', password: 'password', password_confirmation: 'password', confirmed_at: Time.now.utc)
   user.skip_confirmation!
   user.skip_confirmation_notification!
+  user.add_role :user
   user.save
 end
+
+
